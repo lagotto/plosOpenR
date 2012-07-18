62bc2c1236422dea4e51803f2933ec34211b901c
Martin Fenner <mfenner@sciencecard.org>
2012-07-19 00:19:06 +0200

    added formatting to save as pdf file.

--- bubbleChart.R
+++ bubbleChart.R
@@ -1,6 +1,9 @@
-# Bubble chart from PLoS Article Level Metrics data
-# Version 1.1, 07/07/12
-# by Martin Fenner, mf@martinfenner.org
+#' Bubble chart for PLoS Article Level Metrics
+#'
+#' @author Martin Fenner, mfenner@plos.org
+
+# Utility functions
+today <- format(Sys.Date(), "%Y-%m-%d")
 
 # Load data
 alm <- read.csv("alm_out.csv", header=TRUE)
@@ -9,17 +12,23 @@ y <- alm$counter + alm$pmc
 z <- sqrt( alm$crossref / pi )
 
 # Labels
-main <- "Seventh Framework Programme (FP7)"
 today <- format(Sys.Date(), "%B %e, %Y")
-sub <- sprintf("PLoS Article Level Metrics Report (%s)", today)
+plos.title <- "Title"
+plos.description <- "Description"
 plos.xlab <- "Months"
 plos.ylab <- "Cumulative Views"
-main_color <- "#789aa1"
-label_color <- "#304345"
+plos.color <- "#789aa1"
   
+# Plot the chart. Results are saved in the "data-charts" subdirectory
+plos.data_charts <- "data-charts"
+plos.pdf <- paste(plos.data_charts, "/", "bubbblechart", "_", today, ".pdf", sep="")
+pdf(file=plos.pdf, width=8, height=6, title=plos.title, useDingbats=FALSE)
 
-# Plot the chart
-opar <- par(mai=c(1.5,1.5,1.5,1.5), mgp=c(3.5,0.75,0), fg="black", cex.main=2, cex.sub=1.25, cex.lab=1.5, col="white", col.main=label_color, col.lab=label_color, xaxs="i", yaxs="i")
-symbols(x / 365.25 * 12, y, circles=z, inches=0.25, bg=main_color, main=main, xlab=plos.xlab, ylab=plos.ylab, las=1)
-#text(x / 365.25 * 12, views, alm$doi, pos=3, offset=0.75, cex=0.5)
+opar <- par(mai=c(0.5,.75,0,0.5), omi=c(0.5,0.5,1.5,0.5), mgp=c(2,0.75,0), fg="black", cex.main=2, cex.lab=1.5, col="white", col.main=plos.color, col.lab=plos.color, xaxs="i", yaxs="i")
+symbols(x / 365.25 * 12, y, circles=z, inches=0.25, bg=plos.color, xlab=NA, ylab=NA, las=1)
+title(main=plos.title, cex.main=2, outer=TRUE, line=4, adj=0)
+mtext(paste(strwrap(plos.description,width=90), collapse="\n"), side=3, col="black", outer=TRUE, line=1.5, adj=0)
+mtext(plos.xlab, side=1, col=plos.color, cex=1.25, outer=TRUE, adj=1, at=1)
+mtext(plos.ylab, side=2, col=plos.color, cex=1.25, outer=TRUE, adj=0, at=1, las=1)
 par(opar)
+dev.off()
