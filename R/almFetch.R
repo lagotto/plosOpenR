76abe2f49e88f1312a3c23cc2a8890ec87ecbe59
Martin Fenner <mf@martinfenner.org>
2012-08-18 19:00:39 +0200

    updated almFetch script to use updated rplos library and ALM 2.0, and turned it into a function

--- R/almFetch.R
+++ R/almFetch.R
@@ -1,107 +1,142 @@
 #' Fetch Article Level Metrics from PLOS
-#' @import rplos stringr
-#' @param articles list of DOIS (dataframe)
+#' @import rplos stringr plyr
+#' @param articles list of DOIs (dataframe)
 #' Provide a dataframe with at least one DOI column. The function calls the 
 #' PLOS ALM API and adds the response to the input dataframe
-#' 
-#' @author Martin Fenner, mfenner@plos.org
+#' @examples \dontrun{
+#' almFetch(articles)
+#' }
+#' @author Martin Fenner <mfenner@plos.org>
 
 almFetch <- function(articles, key=getOption("PlosApiKey")) {
   
-stopifnot (is.data.frame(articles))
-stopifnot (!is.null(articles$doi))
+  # Make sure articles is a dataframe with a "doi" column
+  if(class(articles) == "character") {
+    articles <- as.data.frame(articles)
+    colnames(articles) <- c("doi")
+  } else if(class(articles) == "list") {
+    articles <- as.data.frame(unlist(articles))
+    colnames(articles) <- c("doi")
+  }
+  stopifnot(is.data.frame(articles))
+  names(articles)[names(articles)=="DOI"] <- "doi"
+  stopifnot (!is.null(articles$doi))
 
-# Load required libraries
-library(rplos)
-library(stringr)
-
-alm <- data.frame()
-
-# Loop through all provided DOIs
-for (i in 1:nrow(articles))  {
-  article <- articles[i,]
-  # Calling the PLoS ALM API. Waiting 10 sec before calling the API again.
-  response <- almplosallviews(article$doi, citations = TRUE, history = FALSE, downform='json', sleep=2, key = plos.api_key)
-
-  # Add PMID if it exists
-  article.pmid <- if (is.null(response$article$pub_med)) NA else response$article$pub_med
-  article <- c(article, list(pmid=article.pmid))
-
+  # Load required libraries
+  library(rplos)
+  library(stringr)
+  library(plyr)
+  suppressPackageStartupMessages(library(googleVis))
+  
   # Parse journal name from DOI unless we know the name already
-  if (is.null(article$journal)) {
-    plos.journals <- list(pbio="PLoS Biology", 
-                          pmed="PLoS Medicine",
-                          pone="PLoS ONE",
-                          ppat="PLoS Pathogens",
-                          pcbi="PLoS Computational Biology",
-                          pntd="PLoS Neglected Tropical Diseases",
-                          pgen="PLoS Genetics",
-                          pctr="PLoS Clinical Trials")
-    
-    journal.key <- substr(article$doi,17,20)
-    article.journal <- plos.journals[[journal.key]]
-    article <- c(article, list(journal=article.journal))
-  } 
-  
-  # Parse publication date unless we know it already
-  if (is.null(article$publication_date)) {
-    article.publication_date <- as.Date(response$article$published)
-    article <- c(article, list(publication_date=article.publication_date))
-  }
-  
-  # Parse title from response unless we know it already
-  if (is.null(article$title)) {
-    article.title <- gsub("<italic>", "", response$article$title)
-    article.title <- gsub("</italic>", "", article.title)
-    article.title <- iconv(article.title, from = "latin1", to = "UTF-8")
-    article.title <- gsub("\n", " ", article.title, fixed=TRUE)
-    article.title <- gsub("                    ", "", article.title, fixed=TRUE)
-    article.title <- str_trim(article.title)
-    article <- c(article, list(title=article.title))
-  }
-  
-  lst <- list()
-  
-  # Add citation_counts from sources. We will not use all sources, and need special parsing for some sources.
-  for (source in response$article$source) {
-    source.name <- tolower(source$source)
-    if (source.name == "citeulike" || 
-        source.name == "crossref" ||
-        source.name == "scopus" ||
-        source.name == "twitter") {
-      lst[source.name] <- source$count
-    } else if (source$source == "PubMed Central") {
-      lst["pubmed"] <- source$count
-    } else if (source.name == "counter") {
-      views <- source$citations[[1]]$citation$views
-      pdf_views <- sum(as.numeric(sapply(views, `[[`, "pdf_views")))
-      html_views <- sum(as.numeric(sapply(views, `[[`, "html_views")))
-      xml_views <- sum(as.numeric(sapply(views, `[[`, "xml_views")))
-      lst["counter_pdf"] <- pdf_views
-      lst["counter_html"] <- html_views
-      lst["counter_xml"] <- xml_views
-    } else if (source$source== "PubMed Central Usage Stats") {
-      views <- source$citations[[1]]$citation$views
-      pdf <- sum(as.numeric(sapply(views, `[[`, "pdf")))
-      html <- sum(as.numeric(sapply(views, `[[`, "full-text")))
-      lst["pmc_pdf"] <- pdf
-      lst["pmc_html"] <- html
-    } else if (source.name == "facebook") {
-      doi_count <- if(is.null(source$citations[[1]]$citation$total_count)) 0 else source$citations[[1]]$citation$total_count
-      fulltext_count <- if(is.null(source$citations[[2]]$citation$total_count)) 0 else source$citations[[2]]$citation$total_count
-      lst[source.name] <- doi_count + fulltext_count
-    } else if (source.name == "mendeley") {
-      citation <- source$citations[[1]]$citation
-      readers <- if(is.null(citation$stats)) 0 else citation$stats$readers
-      groups <- if(is.null(citation$groups)) 0 else length(citation$groups)
-      lst["mendeley_readers"] <- readers
-      lst["mendeley_groups"] <- groups
-    } else {
-      next
+  if (is.null(articles$journal)) {
+    getJournal <- function(doi) {
+      plos.journals <- c(pbio="PLoS Biology", 
+                            pmed="PLoS Medicine",
+                            pone="PLoS ONE",
+                            ppat="PLoS Pathogens",
+                            pcbi="PLoS Computational Biology",
+                            pntd="PLoS Neglected Tropical Diseases",
+                            pgen="PLoS Genetics",
+                            pctr="PLoS Clinical Trials")
+      plos.journals[[substr(doi,17,20)]]
     }
+    articles$journal <- daply(articles, "doi", getJournal)
   }
-  article <- c(article, lst)
-  response <- rbind(alm, data.frame(article))
-}
-response
+  
+  alm <- data.frame()
+    
+  # Loop through all provided DOIs
+  for (i in 1:nrow(articles))  {
+    article <- articles[i,]
+    # Calling the PLoS ALM API. Waiting 10 sec before calling the API again.
+    response <- almplosallviews(article$doi, events=1, downform='json', sleep=0, key = key)
+    
+    # Start with the DOI, needed for merging
+    result <- list(doi=article$doi)
+    
+    # Add PMID if it exists
+    article.pmid <- if (is.null(response$article$pub_med)) NA else as.numeric(response$article$pub_med)
+    result <- c(result, list(pmid=article.pmid))
+    
+    # Parse publication date unless we know it already
+    if (is.null(article$publication_date)) {
+      article.publication_date <- as.Date(response$article$published)
+      result <- c(result, list(publication_date=article.publication_date))
+    }
+    
+    # Parse title from response unless we know it already
+    if (is.null(article$title)) {
+      article.title <- gsub("<italic>", "", response$article$title)
+      article.title <- gsub("</italic>", "", article.title)
+      article.title <- gsub("<sub>", "", article.title)
+      article.title <- gsub("</sub>", "", article.title)
+      article.title <- gsub("<sup>", "", article.title)
+      article.title <- gsub("</sup>", "", article.title)
+      article.title <- iconv(article.title, from = "latin1", to = "UTF-8")
+      article.title <- gsub("\n", " ", article.title, fixed=TRUE)
+      article.title <- gsub("                    ", "", article.title, fixed=TRUE)
+      article.title <- str_trim(article.title)
+      result <- c(result, list(title=article.title))
+    }
+    
+    lst <- list()
+    
+    # Add event_counts from sources. We will not use all sources, and need special parsing for some sources.
+    for (source in response$article$source) {
+      source.name <- tolower(source$source)
+      if (source.name == "crossref" ||
+        source.name == "scopus" ||
+        source.name == "citeulike" ||
+        source.name == "twitter") {
+        lst[source.name] <- source$count
+      } else if (source$source == "PubMed Central") {
+        lst["pubmed"] <- source$count
+      } else if (source.name == "counter") {
+        pdf_views <- sum(as.numeric(sapply(source$events, `[[`, "pdf_views")))
+        html_views <- sum(as.numeric(sapply(source$events, `[[`, "html_views")))
+        xml_views <- sum(as.numeric(sapply(source$events, `[[`, "xml_views")))
+        lst["counter_pdf"] <- pdf_views
+        lst["counter_html"] <- html_views
+        lst["counter_xml"] <- xml_views
+        lst["counter"] <- source$count
+      } else if (source$source== "PubMed Central Usage Stats") {
+        views <- source$citations[[1]]$citation$views
+        pdf <- sum(as.numeric(sapply(source$events, `[[`, "pdf")))
+        html <- sum(as.numeric(sapply(source$events, `[[`, "full-text")))
+        lst["pmc_pdf"] <- pdf
+        lst["pmc_html"] <- html
+        lst["pmc"] <- source$count
+      } else if (source$source == "Research Blogging") {
+        lst["researchblogging"] <- source$count
+      } else if (source.name == "facebook") {
+        if (source$count > 0) {
+          lst["facebook_shares"] <- sum(as.numeric(sapply(source$events, `[[`, "share_count")))
+          lst["facebook_likes"] <- sum(as.numeric(sapply(source$events, `[[`, "like_count")))
+          lst["facebook_comments"] <- sum(as.numeric(sapply(source$events, `[[`, "comment_count")))
+          lst["facebook_clicks"] <- sum(as.numeric(sapply(source$events, `[[`, "click_count")))
+          lst["facebook"] <- source$count
+        } else {
+          lst["facebook_shares"] <- 0
+          lst["facebook_likes"] <- 0
+          lst["facebook_comments"] <- 0
+          lst["facebook_clicks"] <- 0
+          lst["facebook"] <- 0
+        }
+      } else if (source.name == "mendeley") {
+        readers <- if(is.null(source$events)) 0 else source$events$stats$readers
+        groups <- if(is.null(source$events) || is.null(source$events$groups)) 0 else length(source$events$groups)
+        lst["mendeley_readers"] <- readers
+        lst["mendeley_groups"] <- groups
+        lst["mendeley"] <- source$count
+      } else {
+        next
+      }
+    }
+    result <- c(result, lst)
+    alm <- rbind(alm, data.frame(result))
+  }
+  
+  # Merge with input dataframe and return the result
+  alm <- merge(articles, alm, by="doi", all.x=TRUE)
 }
--- R/dotChart.R
+++ R/dotChart.R
@@ -9,11 +9,12 @@ dotchart <- function(x1, x2, x3, xlab, x3lab, labels, main, description, col="#7
   library(stringr)
 
   xmax <- max(x2) * 1.05
-  labels <- str_sub(labels, start=1, end=80)
+  labels <- str_sub(labels, start=1, end=90)
+  cex.labels <- 0.75
 
   opar <- par(mai=c(.5,0,.75,0.5), omi=c(.5,.5,.5,.5), fin=c(10,7.5), lty=0)
-  dotchart2(x1, labels=labels, auxdata=x3, auxtitle=x3lab, dotsize=1, lty=3, pch=1, xlim=c(0,xmax), reset.par=TRUE)
-  dotchart2(x2, labels=labels, dotsize=1, pch=19, xlim=c(0,xmax), add=TRUE)
+  dotchart2(x1, labels=labels, cex.labels=cex.labels, auxdata=x3, auxtitle=x3lab, dotsize=1, lty=3, pch=1, xlim=c(0,xmax), reset.par=TRUE)
+  dotchart2(x2, labels=labels, cex.labels=cex.labels, dotsize=1, pch=19, xlim=c(0,xmax), add=TRUE)
 
   title(main=main, cex.main=2, col.main=col, outer=TRUE, line=0, adj=0)
   mtext(paste(strwrap(description,width=120), collapse="\n"), side=3, col="black", cex=1, outer=TRUE, line=-2.75, adj=0, xpd=TRUE)
