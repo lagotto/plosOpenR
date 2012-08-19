1da90a002c8060f0bb3e8c0a87bc375fa147373e
Martin Fenner <mf@martinfenner.org>
2012-08-19 16:40:20 +0200

    added optional lowess line to scatterplot function

--- R/scatterPlot.R
+++ R/scatterPlot.R
@@ -2,7 +2,7 @@
 #'
 #' @author Martin Fenner <mfenner@plos.org>
 
-scatterplotp <- function(x, y, z=NULL, xlab=NULL, ylab=NULL, log="", main=NULL, description=NULL, col="#789aa1") {
+scatterplotp <- function(x, y, z=NULL, xlab=NULL, ylab=NULL, log="", main=NULL, description=NULL, lowess=FALSE, col="#789aa1") {
 
   # Load required libraries
   library(stats)
@@ -15,12 +15,12 @@ scatterplotp <- function(x, y, z=NULL, xlab=NULL, ylab=NULL, log="", main=NULL, 
   colPal <- col #brewer.pal(9,"Blues")
 
   # Linear regression
-  # alm[is.na(alm)] <- 0
-  # lmfit <- lm(alm[[plos.y]] ~ alm[[plos.x]], na.action=na.omit, subset=alm[[plos.x]] <= plos.fitmax)
+  lmfit <- lm(y ~ x, na.action=na.omit)
   # panel.first=c(abline(lmfit, col="#ff6666", lwd=3))
   
   opar <- par(mai=c(0.5,.75,0.25,0.5), omi=c(0.5,0.5,1.5,0.5), mgp=c(3,0.75,0), fg="black", cex.main=2, cex.lab=1.5, col=col, col.main=col, col.lab=col)
   plot(x, y, log=log, type="p", pch=21, col=col, bg=col, xlim=c(1,xmax), ylim=c(1,ymax), xlab="", ylab="", frame.plot=FALSE, las=1)
+  if(lowess) lines(lowess(x,y), col="#a17f78", lwd=3)
   title(main=main, cex.main=2, col.main=col, outer=TRUE, line=4, adj=0)
   mtext(paste(strwrap(description,width=120), collapse="\n"), side=3, col="black", cex=1, outer=TRUE, line=1.5, adj=0, xpd=TRUE)
   mtext(xlab, side=1, col=col, cex=1.25, outer=TRUE, adj=1, at=1)
